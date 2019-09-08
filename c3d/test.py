e907cd186fcea1092eafdb3316b5ccf4972453bf
hungtran1196 <hungtran1196@gmail.com>
2019-09-08 18:25:51 +0700

    refactor

--- c3d/test.py
+++ c3d/test.py
@@ -1,47 +1,38 @@
 import os
 import json
 import time
+import argparse
+from pathlib import PurePath
 
 import numpy as np
 import cv2
 import torch
 from torchvision import transforms
 
-from utils.ucf.transform_ucf50 import Rescale, CenterCrop, ToTest
+from utils.ucf.transform import Rescale, CenterCrop, ToTest
+import config
 
 
-location = "cuda:0" if torch.cuda.is_available() else "cpu"
-device = torch.device(location)
-print("Device being used:", device)
+def main(model_path, video_path):
+    # Use cuda if it is possible
+    location = "cuda:0" if torch.cuda.is_available() else "cpu"
+    device = torch.device(location)
+    print("Device being used:", device)
 
-
-if __name__ == "__main__":
-    model_path = 'models_ucf50_final.pth' 
-    test_video_path = 'test4.mp4'
-    with open("./dataset/categories.json", "r") as read_file:
-        categories = json.load(read_file)
-
-    labels, label_names = categories.values(), categories.keys()
-    labels, label_names = zip(*sorted(zip(labels, label_names)))
-    print(label_names)
-
-
-
-    model = torch.load(model_path, map_location=location)
+    with open(config.training['category_filepath']) as f:
+        categories = json.load(f)
+    
+    model = torch.load(model_path, map_location=device)
     model.to(device)
+    model.eval()
 
-    model.eval()   
-
-    cap = cv2.VideoCapture(test_video_path)
+    cap = cv2.VideoCapture(video_path)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-
-    print(cap.get(3), cap.get(4))
-    out = cv2.VideoWriter('output_test4.avi',fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
-
-    frame_id = 0
-    frame_per_clip = 16
-
-    clip = []
+    out = cv2.VideoWriter(os.path.join(config.testing['output_folder'], 'result_{}.avi'.format(PurePath(video_path).name)),
+                        fourcc, 
+                        20.0, 
+                        (int(cap.get(3)), int(cap.get(4))),
+                        )
 
     test_transform = transforms.Compose([
         Rescale((171, 128)),
@@ -50,37 +41,40 @@ if __name__ == "__main__":
     ])
 
     category = 'unknown'
+    probability = 0
+    num_frame = 0
+    clip = []
     while True:
         ret, frame = cap.read()
-
         if ret is None:
             break
 
-        frame_id += 1
+        num_frame += 1
         clip.append(frame)
-
-        if frame_id >= 16:
+           
+        start_time = time.time()
+        if num_frame >= 16:
             clip_tensor = test_transform(np.array(clip))
             clip_tensor.to(device)
 
-            start_time = time.time()
             output = model(clip_tensor)
-            run_time = time.time() - start_time
             probs = torch.nn.Softmax(dim=1)(output)
             label = torch.max(probs, 1)[1].detach().cpu().numpy()[0]
-
-            cv2.putText(frame, label_names[label], (20, 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6,
-                        (0, 0, 255), 2)
-            cv2.putText(frame, "prob: %.4f" % probs[0][label], (20, 40),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6,
-                        (0, 0, 255), 2)
-
-            cv2.putText(frame, "fps: %.4f" % (1 / run_time), (20, 60),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6,
-                        (0, 0, 255), 2)
-
+            category = categories[label]
+            probability = probs[0][label]
             clip.pop(0)
+
+        run_time = time.time() - start_time
+        cv2.putText(frame, category, (20, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                    (0, 0, 255), 2)
+        cv2.putText(frame, "prob: %.4f" % probability, (20, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                    (0, 0, 255), 2)
+
+        cv2.putText(frame, "fps: %.4f" % (1 / run_time), (20, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                    (0, 0, 255), 2)
 
         out.write(frame)
         cv2.imshow('result', frame)
@@ -90,3 +84,11 @@ if __name__ == "__main__":
     out.release()
 
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Performing action recognition on videos')
+    parser.add_argument('--model', help='Location of model file')
+    parser.add_argument('--video', help='Location of video file')
+
+    args = parser.parse_args()
+    main(args.model, args.video)
